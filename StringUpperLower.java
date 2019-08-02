55e921d434ad3a0e8ae1a41546127846d76fbe4b
Aaron Hodges <53194734+aaronhodges1126@users.noreply.github.com>
2019-08-02 14:33:58 -0500

    Create StringUpperLower.java

---
+++ StringUpperLower.java
@@ -0,0 +1,7 @@
+public class MyClass {
+  public static void main(String[] args) {
+    String txt = "Hello from Aaron";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }
+}
